9e9558969f20553eee4cdb2ef46c19fe7675831d
7k1aeu <takkeyito@yahoo.co.jp>
2025-11-21 21:52:11 +0900

    Delete convert_to_ts.py

--- convert_to_ts.py
+++
@@ -1,8 +0,0 @@
-from glyphs import GLYPHS
-
-with open('glyphs.ts', 'w', encoding='utf-8') as f:
-    f.write("export const GLYPHS: { [key: string]: number[] } = {\n")
-    for k, v in GLYPHS.items():
-        arr = ', '.join(f'0x{col:04x}' for col in v)
-        f.write(f"  '{k}': [{arr}],\n")
-    f.write("};\n")
